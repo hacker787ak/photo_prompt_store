ae3614a217567ba1eb4dfa0a4824a81f6953f4c0
Akshay <akshay@example.com>
2026-04-01 23:55:12 +0530

    Added admin creation script

--- create_admin.py
+++ create_admin.py
@@ -5,3 +5,6 @@ django.setup()
 from django.contrib.auth.models import User
 if not User.objects.filter(username='hacker787ak').exists():
     User.objects.create_superuser('hacker787ak', 'akshay@example.com', 'Password123@')
+    print("Admin created successfully!")
+else:
+    print("Admin already exists.")
