2153e062ab9d824372f2ad56f2fc0cbda5484c74
Akshay <akshay@example.com>
2026-04-03 07:40:39 +0530

    Auto-create admin via build script

--- create_admin.py
+++ create_admin.py
@@ -3,5 +3,13 @@ import django
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'photoprompt.settings')
 django.setup()
 from django.contrib.auth.models import User
-if not User.objects.filter(username='hacker787ak').exists():
-    User.objects.create_superuser('hacker787ak', 'akshay@example.com', 'Password123@')
+
+username = 'hacker787ak'
+email = 'akshay@example.com'
+password = 'Password1234@'
+
+if not User.objects.filter(username=username).exists():
+    User.objects.create_superuser(username, email, password)
+    print(f"Superuser {username} created successfully!")
+else:
+    print(f"Superuser {username} already exists.")
